a543523b63202cdd07417edb04ebf9efeeb5f92d
Mahdi Farzadi <mahdifarzadi78@gmail.com>
2019-11-07 12:46:46 +0330

    create project 2

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 
 urlpatterns = [
     path('', views.navbar),
+    path('register/', views.sign_in),
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def navbar(request):
     return render(request, 'main/home.html')
+
+def sign_in(request):
+    return render(request, 'main/register.html')
