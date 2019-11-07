948853db6af666d903cfd34eb2013a6836583a53
Mohammad <mohammad.ce79@gmail.com>
2019-11-07 14:07:48 +0330

    fix register add logout add superuser mohammad

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 
 from app import views
 
 urlpatterns = [
     path('', views.navbar),
     path('register/', views.sign_in),
+    path('logout/', views.logout_request),
 ]
