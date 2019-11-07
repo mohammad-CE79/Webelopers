26600ee1a4febcf2967253a67c5b923bff059236
Mohammad <mohammad.ce79@gmail.com>
2019-11-07 14:22:21 +0330

    delete super user

--- app/views.py
+++ app/views.py
@@ -9,19 +9,18 @@ def navbar(request):
 
 
 def sign_in(request):
-    # return render(request, 'main/register.html')
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            # messages.success(request, f"new account created : {username}")
+            messages.success(request, f"new account created : {username}")
             login(request, user)
             return redirect("/")
 
-            # else:
-            #     for msg in form.error_messages:
-            #         messages.error(request, f"{msg}: {form.error_messages[msg]}")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
             return render(request=request,
                           template_name='main/register.html',
                           context={"form": form})
