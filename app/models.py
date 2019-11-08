9aa185f68398133940465ff4713b9634e8798526
Mohammad <mohammad.ce79@gmail.com>
2019-11-08 09:34:48 +0330

    bug fix 5

--- app/models.py
+++ app/models.py
@@ -10,12 +10,12 @@ class Course(models.Model):
     teacher = models.CharField(max_length=200)
     start_time = models.TimeField()
     end_time = models.TimeField()
-    first_day = models.IntegerField(choices=[0, 1, 2, 3, 4])
-    second_day = models.IntegerField(choices=[0, 1, 2, 3, 4])
+    # first_day = models.IntegerField(choices=[0, 1, 2, 3, 4])
+    # second_day = models.IntegerField(choices=[0, 1, 2, 3, 4])
 
 
 class CourseForm(ModelForm):
     class Meta:
         model = Course
-        fields = ['department', 'name', 'course_number', 'group_number',
-                  'teacher', 'start_time', 'end_time', 'first_day', 'second_day']
+        fields = ['department', 'name', 'course_number', 'group_number',]
+                  # 'teacher', 'start_time', 'end_time', 'first_day', 'second_day']
